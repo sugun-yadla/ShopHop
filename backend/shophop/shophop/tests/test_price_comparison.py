0c17baa9293e20ac2d832a0bb3276d7bc60e49a7
Gargeeshah <gargeeshah@gmail.com>
2024-12-09 20:18:13 -0500

    test code

--- backend/shophop/shophop/tests/test_price_comparison.py
+++ backend/shophop/shophop/tests/test_price_comparison.py
@@ -33,13 +33,13 @@ class ProductFetchTests(unittest.TestCase):
         response = self.client.get(reverse('fetch-products', args=[product_query]))
         response_data = response.json()
         #print(response.status_code)
-        assert response.status_code != 200
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
     
     def test_multiple_products(self):
         product_query = "milk, cheese, birthfydy"
         response = self.client.get(reverse('fetch-products', args=[product_query]))
         response_data = response.json()
-        assert response.status_code == 200
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
     
     def test_only_show_data_having_all_fields(self):
         mock_test_data = pd.DataFrame([{'Product': 'fairlife Lactose Free Fat Free Ultra Filtered Milk', 'Price': None, 'Quantity': None, 'Category': 'milk', 'store': 'Walmart'}, 
@@ -56,16 +56,14 @@ class ProductFetchTests(unittest.TestCase):
         filtered_data = price_comparison.data_cleaning(mock_test_data)
         response_data = json.loads(filtered_data.content) 
         filtered_data_df = pd.DataFrame(response_data)
-        assert filtered_data_df.isnull().sum().sum() == 0, "Filtered data contains None or NaN values"
+        self.assertEqual(filtered_data_df.isnull().sum().sum() == 0, "Filtered data contains None or NaN values")
 
     # to add
     # check if None getting assigned properly through automation
-    # comparison
     
 class StandardizeQuantityTests(unittest.TestCase):
-
-    def test_valid_quantities(self):
-        mock_test_data = pd.DataFrame([{'Product': 'Mavuno Harvest Organic Dried Fruit', 'Price': 3.64, 'Quantity': '2 Ozbag', 'Category': 'banana', 'store': 'Walmart', 'expected': '2.00 oz'}, 
+    def setUp(self):
+        self.mock_test_data = pd.DataFrame([{'Product': 'Mavuno Harvest Organic Dried Fruit', 'Price': 3.64, 'Quantity': '2 Ozbag', 'Category': 'banana', 'store': 'Walmart', 'expected': '2.00 oz'}, 
                                  {'Product': 'Nature All Foods Organic Freeze Dried Raw Banana 2.5 oz - Vegan', 'Price': 7.88, 'Quantity': 'Nature All Foods Organic Freeze Dried Raw Banana 2.5 oz - Vegan', 'Category': 'banana', 'store': 'Walmart', 'expected': '2.50 oz'}, 
                                  {'Product': 'Silk Dairy Free', 'Price': 3.34, 'Quantity': '4 Pack', 'Category': 'milk', 'store': 'Walmart', 'expected':'4.00 ct'}, 
                                  {'Product': 'sugar', 'Price': 4.97, 'Quantity': '1.5 lb', 'Category': 'sugar', 'store': 'Walmart', 'expected':'24.00 oz'}, 
@@ -74,7 +72,9 @@ class StandardizeQuantityTests(unittest.TestCase):
                                  {'Product': 'Great Value Whole Vitamin D Milk', 'Price': 3.52, 'Quantity': '2 gallons', 'Category': 'milk', 'store': 'Aldi', 'expected': '256.00 oz'},
                                  {"Product": "Great Value Milk 1 Lowfat Half Gallon Plastic Jug","Price": 2.12, "Quantity": "Great Value Milk 1 Lowfat Half Gallon Plastic Jug","Category": "milk","store": "Walmart","Standardized_Quantity": None}])
         
-        for _, row in mock_test_data.iterrows():
+    def test_valid_quantities(self):
+        
+        for _, row in self.mock_test_data.iterrows():
             result = price_comparison.standardize_quantity(row)
             self.assertTrue(pd.isna(result) and pd.isna(row['expected']) or result == row['expected'])
     
@@ -85,6 +85,9 @@ class StandardizeQuantityTests(unittest.TestCase):
         expected_response = {"message": "No valid data after cleaning."}
         self.assertEqual(clean_data.status_code, 200)
         self.assertEqual(actual_response, expected_response)
-            
+    
+    def test_price_comparison(self):
+        sorted_data = price_comparison.priceComparison(self.mock_test_data)
+        print(sorted_data)
 if __name__ == "__main__":
     unittest.main()
